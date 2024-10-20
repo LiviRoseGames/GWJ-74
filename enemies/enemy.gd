a2fae6322b4af2499685768702b98856e669c8ee
LiviRose <livirosegames@gmail.com>
2024-10-20 04:19:38 -0700

    Late Night Work

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,12 +2,11 @@ class_name Enemy
 extends CharacterBody2D
 
 @export var movement_stats: MovementStats
-
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var hitbox: Hitbox = $Hitbox
 @onready var hurtbox: Hurtbox = $Hurtbox
+@onready var flip_anchor: Node2D = $FlipAnchor
 
 func _ready() -> void:
 	motion_mode = MOTION_MODE_FLOATING
